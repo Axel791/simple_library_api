187d1bbff26c663392d1c9c0cde695370a20dd64
Axel791 <HS.Kazakov@mail.ru>
2023-02-06 22:57:07 +0300

    починил update для юзера

--- api_django/users/serializers.py
+++ api_django/users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from users.models import UserProfile
+from utils.errors import STATUS_NOT_FOUND
 
 
 class UsersSerializer(serializers.ModelSerializer):
@@ -25,11 +26,21 @@ class UsersSerializer(serializers.ModelSerializer):
         )
 
     def update(self, instance, validated_data):
-        instance.fist_name = validated_data.get("first_name", instance.first_name)
-        instance.last_name = validated_data.get("last_name", instance.last_name)
-        instance.user_status = validated_data.get("user_status", instance.user_status)
+        user_data = validated_data.pop('user')
+        user = instance.user
+        for k, v in user_data.items():
+            setattr(user, k, v)
         instance.address = validated_data.get("address", instance.address)
+        instance.user_status = validated_data.get("user_status", instance.user_status)
+        user.save()
         instance.save()
         return instance
+
+    def validate_user_status(self, data):
+        if data not in [UserProfile.ORDINARY_USER, UserProfile.LIBRARY_ASSISTANT, UserProfile.MODERATOR]:
+            return serializers.ValidationError(STATUS_NOT_FOUND)
+        return data
+
+
 
 
